65645215eb8f9cd148059ce05c0bd4a859c01f79
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-13 16:08:06 +0200

    Added Camera.cpp

--- CornellBox/include/Camera.h
+++ CornellBox/include/Camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H value
 
 #include "glm/glm.hpp"
+#include "Pixels.h"
 
 class Camera
 {
---
+++ CornellBox/src/Camera.cpp
@@ -0,0 +1,44 @@
+#include "../include/Camera.h"
+
+/* 	
+	Class Camera  
+	
+	private members:
+	- glm::vec3 position;
+	- glm::vec3 direction;
+	- float viewPlaneDistance;
+	- int resolutionX;
+	- int resolutionY;
+	- int raysPerPixel;
+	- Pixel* pixels;
+*/
+
+/* Default Constructor */
+Camera::Camera()
+{	
+	//TODO: Write code 
+}
+
+/* Destructor */
+Camera::~Camera()
+{
+	//TODO: Write code 
+}
+
+/* Loops over all pixels and computes their values. */
+void Camera::renderImage()
+{
+
+}
+
+/* Converts radiometric values into photometric ones. */
+void Camera::mappingFunction()
+{
+
+}
+
+/* Alternatively displayImage() */						 
+void Camera::saveImage()
+{
+
+}

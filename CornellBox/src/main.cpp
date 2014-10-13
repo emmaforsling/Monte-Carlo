297f421379a90f4e21aa6bb24d93af621767c559
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-13 11:43:25 +0200

    Hello people all around the world, Nu kör vi

---
+++ CornellBox/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	// char* argument1 = argv[0];
+	// int argument2 = argc;
+	
+	std::cout << "Hello World" << std::endl;
+	//Create Sceen
+
+	//Loop over cols and rows
+
+	//Write to file
+}
+
+

737a3da870bf49ca1d75f32c6d0dafc52bea38a7
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-13 15:59:45 +0200

    Wall.cpp

---
+++ CornellBox/src/Wall.cpp
@@ -0,0 +1,36 @@
+#include "../include/Wall.h"
+
+/* 	
+	Class Wall  
+	
+	private members:
+	- glm::vec3 position;
+	- float size;
+*/
+
+/* Default Constructor */
+Wall::Wall()
+{	
+	//TODO: Write code 
+}
+
+/* Destructor */
+Wall::~Wall()
+{
+	//TODO: Write code 
+}
+
+/* Sets corners of the Rectangle to (0,0,0), (1,0,0), (0,1,0) etc. 
+	and multiplies them by size (float) and adds the position 
+	(glm::vec3) */
+	
+void Wall::initializeRectangles()
+{
+
+}
+
+/* either one intersection or none (ray leaving) */				
+glm::vec3 Wall::calculateIntersection()
+{
+	
+}			

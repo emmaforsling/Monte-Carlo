d28bf6dda109a07bb905155205d3324a6e72f384
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-15 17:36:52 +0200

    checkpoint, added comments in the sphere class on what todo

--- CornellBox/src/Sphere.cpp
+++ CornellBox/src/Sphere.cpp
@@ -34,8 +34,21 @@ Sphere::~Sphere()
 	//TODO: Write code 
 }
 
-glm::vec3 Sphere::calculateIntersection()
-{
+/*
+	This function should calculate the intersectionpoints between a ray 
+	and the surface of the Sphere. //For the surface of the sphere, use the radius
+	and the centerpoint. //
+	Zero intersection corresponds to a miss
+	One intersection corresponds to tangential hit
+	If(two)
+		If(Outside the sphere)
+			//determine correct intersectionPoint
+			compare the length of the vectors from the ray's 
+			origin to the two intersection points
+			Select the closer one....
+		If(Inside the sphere)
+*/
+glm::vec3 Sphere::calculateIntersection(Ray* ray{
 	//TODO: Write code
 	return glm::vec3(0.0,0.0,0.0);
 }

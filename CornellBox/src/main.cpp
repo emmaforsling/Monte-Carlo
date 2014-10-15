e7734397f6fffac4750f6e252e8c24b0a161151b
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-15 21:52:24 +0200

    Structured the main file

--- CornellBox/src/main.cpp
+++ CornellBox/src/main.cpp
@@ -6,24 +6,47 @@ int main(int argc, char *argv[])
 	// char* argument1 = argv[0];
 	// int argument2 = argc;
 	
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello Martin, time to create a beautiful cornell boss" << std::endl;
 
-	//Initialize variables
-	float size = 5.0;
+	/* 
+		Initialize variables 
+	*/
+		// Eye
+	float eyeDistance = 2.0;
+	
+		// Room
+	float size = 5.0;		// Is also used in lightsource, where it is scaled
 	glm::vec3 positionRoom = glm::vec3(0.0, 0.0, 0.0);
-	glm::vec3 positionLight = glm::vec3(0.0, 0.0, 0.0);
-	glm::vec3 positionCube = glm::vec3(0.0, 0.0, 0.0);
-	float radiance = 1.0;	//emitted radiance Le
 
-	//Create Scene
+		// Light
+	float radiance = 1.0;	//emitted radiance Le
+	glm::vec3 positionLight = glm::vec3(0.0, 0.0, 0.0);
+	
+		// Cube
+	glm::vec3 positionCube = glm::vec3(0.0, 0.0, 0.0);
+		
+		// Sphere
+	glm::vec3 positionSphereSpecular = glm::vec3(0.0, 0.0, 0.0);
+	float radiusForSphereSpecular = 0.5;
+	bool transparencyForSphereSpecular = false;
+	float refractiveIndexForSphereSpecular = 1.5;	//glass
+
+	// glm::vec3 positionSphereTransparent = glm::vec3(0.0, 0.0, 0.0);
+	// float radiusForSphereTransparent = 1.0;
+	// bool transparencyForSphereTransparent = true;
+	// float refractiveIndexForSphereTransparent = 1.5;	//glass
+
+	/*
+		Create Scene
+	*/
 	Wall* room = new Wall(positionRoom, size);
 	Light* lightsource = new Light(positionLight, size, radiance); 
 	Cube* c1 = new Cube(positionCube, 1.0, 0.0, 0.0);
-
-
-
-	float eyeDistance = 2.0;
+	Sphere* sphereSpecular = new Sphere(positionSphereSpecular, radiusForSphereSpecular, transparencyForSphereSpecular, refractiveIndexForSphereSpecular );
+	//Sphere* sphereTransparent = new Sphere(positionSphereTransparent, radiusForSphereTransparent, transparencyForSphereTransparent, refractiveIndexForSphereTransparent);
 	Camera* camera = new Camera(room, eyeDistance, 1);
+	
+
 	//Loop over cols and rows
 
 	//Write to file

41b46b13314896c581a9e61ecc8d112252a87fb1
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-15 19:59:59 +0200

    checkpoint: added comments in calcIntersection in Sphere, which should work

--- CornellBox/src/Sphere.cpp
+++ CornellBox/src/Sphere.cpp
@@ -48,9 +48,40 @@ Sphere::~Sphere()
 			Select the closer one....
 		If(Inside the sphere)
 */
-glm::vec3 Sphere::calculateIntersection(Ray* ray{
+glm::vec3 Sphere::calculateIntersection(Ray* ray){
 	//TODO: Write code
-	return glm::vec3(0.0,0.0,0.0);
+	
+	glm::vec3 intersection = position + glm::vec3(radius,radius,radius);
+	/*
+		Using the equations:
+			S: r^2 = x^2 + y^2 + z^2		//for sphere
+			R = t*(x,y,z)
+			where 
+			t = direction.xyz
+			//förfinar denna text snart
+
+			And rewrote them into 
+			a = (direction.x)^2 + (direction.y)^2 + (direction.z)^2;
+			b = 2*( (direction.x)*(startingPoint.x - centerPoint.x) + 
+					(direction.y)*(startingPoint.y - centerPoint.y) +
+					(direction.z)*(startingPoint.z - centerPoint.z));
+			c = (startingPoint.x - centerPoint.x)^2 +
+				(startingPoint.y - centerPoint.y)^2 +
+				(startingPoint.z - centerPoint.z)^2;
+
+			then 
+			delta = b^2 + 4*a*c;
+
+			if(delta<0) no intersection
+			if(delta==0) one intersection
+			if(delta>0) two intersection
+
+
+
+	*/
+	ray->startingPoint
+
+	return glm::vec3(0.0,0.0,0.0); //returns the point where it intersects
 }
 
 void Sphere::calculateChildRays()

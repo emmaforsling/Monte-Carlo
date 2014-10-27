5c716641d83afa4df3720aab53aa552ea65cc5ae
Emma Forsling <emma.forsling.parborg@gmail.com>
2014-10-27 21:03:53 +0100

    Organized the object class

--- CornellBox/include/Object.h
+++ CornellBox/include/Object.h
@@ -7,21 +7,32 @@
 class Object
 {
 public:
-	Object();
+	// Default empty constructor
+	Object() = default;
+
+	// Constructur
 	Object(glm::vec3 _position, glm::vec3 _color, bool _diffuse, bool _transparent);
-	~Object();
-	virtual glm::vec3 calculateIntersection(Ray* _ray) = 0;
+	
+	// Default destructor
+	~Object() = default;
+
+	// Virtual calculation functions
 	virtual void calculateChildRays(Ray* _ray, glm::vec3 intersectionPoint) = 0;
-	virtual float getRefractiveIndex() = 0;
+	virtual glm::vec3 calculateIntersection(Ray* _ray) = 0;
+	
+	// Virtual get functions
+	virtual glm::vec3 getColor() = 0;
 	virtual glm::vec3 getIntersectedNormal() = 0;
 	virtual int getIntersectedSide() = 0;
-	virtual glm::vec3 getColor() = 0;
+	virtual float getRefractiveIndex() = 0;
+	
+	// Bool functions
 	bool isDiffuse();
 	bool isTransparent();
 
 protected:
-	glm::vec3 position;
 	glm::vec3 color;
+	glm::vec3 position;
 	bool diffuse;
 	bool transparent;
 };

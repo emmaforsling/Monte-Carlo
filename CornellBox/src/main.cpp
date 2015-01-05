51293d6d5be5a8a51eb27e259cf07e97a02b4c1c
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-01-05 16:50:49 +0100

    Changed the sphere location in the scene, now it is in the middle of the room

--- CornellBox/src/main.cpp
+++ CornellBox/src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	double refractiveIndexForCubeSpecular = 1.5;						// glass
 	
 		// Sphere
-	glm::dvec3 positionSphereSpecular = glm::dvec3(3.5, 1.5, 2.5);
+	glm::dvec3 positionSphereSpecular = glm::dvec3(2.5, 2.5, 2.5);
 	double radiusForSphereSpecular = 1.0;
 	bool transparencyForSphereSpecular = false;
 	double refractiveIndexForSphereSpecular = 1.5;						// glass

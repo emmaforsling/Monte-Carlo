9c78be0f7fd3d3ba5e46887aff6958264aa09057
Emma Forsling <emma.forsling.parborg@gmail.com>
2015-01-08 13:34:11 +0100

    Added a setImportance function in Ray.h

--- CornellBox/src/Pixel.cpp
+++ CornellBox/src/Pixel.cpp
@@ -76,7 +76,9 @@ void Pixel::shootRays(glm::dvec3 _cameraPosition, glm::dvec3 _pixelPosition, dou
 		int closestIntersectedObjectIndex;																		// temporary
 		const int numberOfObjects = 2;																			// temporary...
 		int numberOfIterations = 4;																				// number of children
-		int iteration = 0;		
+		int iteration = 0;
+		rays[i]->setImportance(1 / raysPerPixel); //emma 2015-01-08
+		std::cout << "rays[i]->getImportance()" << rays[i]->getImportance() << std::endl;		
 		
 		glm::dvec3 intersectionPoints[numberOfObjects];
 		
